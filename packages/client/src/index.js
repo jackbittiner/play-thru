8e60629bff18fac049882f6a2ddffc3c7887870f
Jack Bittiner <jackbittiner@hotmail.com>
2019-09-17 15:25:41 +0100

    GNT add process environment variables for production

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -11,7 +11,7 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000"
+  uri: process.env.GRAPHQLURI || "http://localhost:4000"
 });
 
 const client = new ApolloClient({
