a5a6c4699978a7e6b2168654bac0ed58e73ada8c
Jack Bittiner <jackbittiner@hotmail.com>
2019-10-01 14:14:32 +0100

    GNT :bug: print .netrc file in travis build for debugging

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,15 @@ setup_git() {
   git config --global user.name "jackbittiner"
 }
 
+display_netrc_file_for_debugging_purposes() {
+  less ~/.netrc
+}
+
 deploy_to_heroku() {
   git push https://git.heroku.com/play-thru-api.git HEAD:master
   git push https://git.heroku.com/play-thru.git HEAD:master
 }
 
+display_netrc_file_for_debugging_purposes
 setup_git
 deploy_to_heroku
