f768acaa6e3652f578c3468f4dc3a4f99744bb11
Jack Bittiner <jackbittiner@hotmail.com>
2019-10-11 17:19:29 +0100

    :white_check_mark: fix test for getTrackById
    Co-authored-by: Toby Hasler-Winter <thaslerwinter@findmypast.com>

--- packages/graphql/resolvers/get-track-by-id.test.js
+++ packages/graphql/resolvers/get-track-by-id.test.js
@@ -1,11 +1,15 @@
-import getCurrentTrack from "./get-current-track";
+import getTrackById from "./get-track-by-id";
 
 describe("getTrackById", function() {
   const spotifyDatasource = {
     get: jest.fn(() => spotifyApiResult)
   };
   it("should resolve the data from spotify", function(done) {
-    return getCurrentTrack("authToken", spotifyDatasource).then(result => {
+    return getTrackById(
+      "authToken",
+      "thisisatrackid00",
+      spotifyDatasource
+    ).then(result => {
       expect(result).toStrictEqual(expectedResult);
       done();
     });
@@ -36,41 +40,36 @@ const artist = {
 };
 
 const spotifyApiResult = {
-  item: {
-    album: {
-      album_type: "album",
-      artists: [artist],
-      external_urls: [],
-      href: "https://api.spotify.com/v1/albums/5AHWNPo3gllDmixgAoFru4",
-      id: "5AHWNPo3gllDmixgAoFru4",
-      images: [{ url: "Image.PNG" }],
-      name: "Heard It In A Past Life",
-      release_date: "2019-01-18",
-      release_date_precision: "day",
-      total_tracks: 12,
-      type: "album",
-      uri: "spotify:album:5AHWNPo3gllDmixgAoFru4"
-    },
+  album: {
+    album_type: "album",
     artists: [artist],
-    disc_number: 1,
-    duration_ms: 188000,
-    explicit: false,
-    external_ids: { isrc: "TCACP1637730" },
-    external_urls: {
-      spotify: "https://open.spotify.com/track/4HfLQJtVT1KiX1eVedDyTm"
-    },
-    href: "https://api.spotify.com/v1/tracks/4HfLQJtVT1KiX1eVedDyTm",
-    id: "4HfLQJtVT1KiX1eVedDyTm",
-    is_local: false,
-    name: "Alaska",
-    popularity: 65,
-    preview_url:
-      "https://p.scdn.co/mp3-preview/c8f67773594c078ad1973f5c46b1b600981043dc?cid=0e58e71063554851870abd7ea374bd45",
-    track_number: 4,
-    type: "track",
-    uri: "spotify:track:4HfLQJtVT1KiX1eVedDyTm"
+    external_urls: [],
+    href: "https://api.spotify.com/v1/albums/5AHWNPo3gllDmixgAoFru4",
+    id: "5AHWNPo3gllDmixgAoFru4",
+    images: [{ url: "Image.PNG" }],
+    name: "Heard It In A Past Life",
+    release_date: "2019-01-18",
+    release_date_precision: "day",
+    total_tracks: 12,
+    type: "album",
+    uri: "spotify:album:5AHWNPo3gllDmixgAoFru4"
   },
-  currently_playing_type: "track",
-  actions: { disallows: { resuming: true } },
-  is_playing: true
+  artists: [artist],
+  disc_number: 1,
+  duration_ms: 188000,
+  explicit: false,
+  external_ids: { isrc: "TCACP1637730" },
+  external_urls: {
+    spotify: "https://open.spotify.com/track/4HfLQJtVT1KiX1eVedDyTm"
+  },
+  href: "https://api.spotify.com/v1/tracks/4HfLQJtVT1KiX1eVedDyTm",
+  id: "4HfLQJtVT1KiX1eVedDyTm",
+  is_local: false,
+  name: "Alaska",
+  popularity: 65,
+  preview_url:
+    "https://p.scdn.co/mp3-preview/c8f67773594c078ad1973f5c46b1b600981043dc?cid=0e58e71063554851870abd7ea374bd45",
+  track_number: 4,
+  type: "track",
+  uri: "spotify:track:4HfLQJtVT1KiX1eVedDyTm"
 };
