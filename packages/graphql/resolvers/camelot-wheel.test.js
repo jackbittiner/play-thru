5d6abb3c90198a1b4027cdbfe4b05db6288b1abc
Jack Bittiner <jackbittiner@hotmail.com>
2019-10-14 14:11:22 +0100

    :white_check_mark: add test for getHarmonicKeys

--- packages/graphql/resolvers/camelot-wheel.test.js
+++ packages/graphql/resolvers/camelot-wheel.test.js
@@ -1,4 +1,4 @@
-import { getKeyName } from "./camelot-wheel";
+import { getKeyName, getHarmonicKeys } from "./camelot-wheel";
 
 describe("getKeyName", function() {
   const keys = [
@@ -10,5 +10,16 @@ describe("getKeyName", function() {
     expect(getKeyName(0, 1, keys)).toEqual("C");
     expect(getKeyName(1, 1, keys)).toEqual("Db");
     expect(getKeyName(2, 1, keys)).toEqual("D");
+  });
+});
+
+describe("getHarmonicKeys", function() {
+  it("should return the harmonic keys for th given key", function() {
+    expect(getHarmonicKeys(0, 1)).toEqual([
+      { name: "C", pitchClass: 0, mode: 1 },
+      { name: "F", pitchClass: 5, mode: 1 },
+      { name: "G", pitchClass: 7, mode: 1 },
+      { name: "Am", pitchClass: 9, mode: 0 }
+    ]);
   });
 });
