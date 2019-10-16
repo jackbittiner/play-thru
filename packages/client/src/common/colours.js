47f0613a3b8638547ed59d1dc511bfefd13844a8
Jack Bittiner <jackbittiner@hotmail.com>
2019-10-16 14:07:59 +0100

    :ambulance: fix compilation error in css

--- packages/client/src/common/colours.js
+++ packages/client/src/common/colours.js
@@ -1,9 +1,9 @@
 const colours = {
-    green: #21FA90,
-    blue: #1C3041,
-    raspberry: #89043D,
-    turquoise: #57E5DE,
-    eucalyptus: #2CEAA3
-}
+  green: "#21FA90",
+  blue: "#1C3041",
+  raspberry: "#89043D",
+  turquoise: "#57E5DE",
+  eucalyptus: "#2CEAA3"
+};
 
-export default colours
+export default colours;
