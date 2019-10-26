8a77d099d3eb18a138b4a2960d6cf30a2831481c
Jack Bittiner <jackbittiner@hotmail.com>
2019-10-26 13:10:14 +0100

    :mag: add meta title and tags to improve SEO

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./pages/login/";
 import { HomePageContainer } from "./pages/home/";
 import ReactGA from "react-ga";
+import { Helmet } from "react-helmet";
 
 ReactGA.initialize("UA-150824990-1");
 
@@ -18,6 +19,17 @@ const Main = () => (
 const App = () => {
   return (
     <div className="app">
+      <Helmet>
+        <title>Playthru - A song recommendation app for DJs</title>
+        <meta
+          name="description"
+          content="A song recommendation app for DJs. Based on the Camelot Wheel and powered by the spotify API, you'll never have a bad mix again"
+        />
+        <meta
+          name="keywords"
+          content="DJ,mixing,camelot,wheel,music,recommendations,harmonic,keys,tempo"
+        />
+      </Helmet>
       <Main />
     </div>
   );
