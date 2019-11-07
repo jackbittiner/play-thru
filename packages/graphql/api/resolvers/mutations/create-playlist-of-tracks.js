8494993b8fd0e85a7df9ec3c9180d6121ef9fee2
Jack Bittiner <jackbittiner@hotmail.com>
2019-11-07 17:25:52 +0000

    :recycle: extract create playlist into own function

--- packages/graphql/api/resolvers/mutations/create-playlist-of-tracks.js
+++ packages/graphql/api/resolvers/mutations/create-playlist-of-tracks.js
@@ -1,10 +1,21 @@
-async function createPlaylistOfTracks(trackUris, userId, spotifyDatasource) {
-  const header = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
+const header = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
 
+async function createPlaylistOfTracks(trackUris, userId, spotifyDatasource) {
+  const playlist = await createPlaylist(userId, spotifyDatasource);
+  await spotifyDatasource.post(
+    `playlists/${playlist.id}/tracks`,
+    {
+      uris: trackUris
+    },
+    header
+  );
+}
+
+export async function createPlaylist(userId, spotifyDatasource) {
   const requestBody = {
     name: "PlayThru Setlist"
   };
@@ -15,13 +26,7 @@ async function createPlaylistOfTracks(trackUris, userId, spotifyDatasource) {
     header
   );
 
-  await spotifyDatasource.post(
-    `playlists/${playlist.id}/tracks`,
-    {
-      uris: trackUris
-    },
-    header
-  );
+  return playlist;
 }
 
 export default createPlaylistOfTracks;
